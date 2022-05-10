bcb5f5c06d9f8f8455d36a335f4f4102b4c065c0
Notavone <notavone@gmail.com>
2022-05-10 12:27:11 +0200

    fix: afficher uniquement les clients non liés
    Signed-off-by: Notavone <notavone@gmail.com>

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,46 +1,48 @@
-import { Injectable } from "@nestjs/common";
-import { CreateClientDto } from "./dto/create-client.dto";
-import { UpdateClientDto } from "./dto/update-client.dto";
-import { InjectConnection, InjectRepository } from "@nestjs/typeorm";
-import { Client } from "./entities/client.entity";
-import { Connection, Repository } from "typeorm";
-import { QueryClientDto } from "./dto/query-client.dto";
+import {Injectable} from "@nestjs/common";
+import {CreateClientDto} from "./dto/create-client.dto";
+import {UpdateClientDto} from "./dto/update-client.dto";
+import {InjectConnection, InjectRepository} from "@nestjs/typeorm";
+import {Client} from "./entities/client.entity";
+import {Connection, Repository} from "typeorm";
 
 @Injectable()
 export class ClientsService {
-  constructor(
-    @InjectRepository(Client) private readonly clientsRepository: Repository<Client>,
-    @InjectConnection() private readonly connection: Connection,
-  ) {
-  }
-
-  create(createClientDto: CreateClientDto) {
-    const client = this.clientsRepository.create(createClientDto);
-    return this.clientsRepository.save(client);
-  }
-
-  findAll(query?: QueryClientDto) {
-    const queryBuilder = this.connection.createQueryBuilder(Client, "client");
-    if (query.clientId) {
-      queryBuilder.where("client.clientId = :clientId", { clientId: query.clientId });
+    constructor(
+        @InjectRepository(Client) private readonly clientsRepository: Repository<Client>,
+        @InjectConnection() private readonly connection: Connection,
+    ) {
     }
-    queryBuilder.leftJoinAndSelect("client.user", "user");
 
-    return queryBuilder.getMany();
-  }
+    findUnlinked() {
+        return this.clientsRepository.find({
+            relations: ['user'],
+            where: {
+                user: null,
+            }
+        });
+    }
 
-  findOne(id: number) {
-    return this.clientsRepository.findOneOrFail(id, {relations: ["user"]});
-  }
+    create(createClientDto: CreateClientDto) {
+        const client = this.clientsRepository.create(createClientDto);
+        return this.clientsRepository.save(client);
+    }
 
-  async update(id: number, updateClientDto: UpdateClientDto) {
-    let client = await this.clientsRepository.findOneOrFail(id, {relations: ["user"]});
-    client = this.clientsRepository.merge(client, updateClientDto);
-    return this.clientsRepository.save(client);
-  }
+    findAll() {
+        return this.clientsRepository.find({relations: ['user']});
+    }
 
-  async remove(id: number) {
-    const client = await this.clientsRepository.findOneOrFail(id);
-    return this.clientsRepository.remove(client);
-  }
+    findOne(id: number) {
+        return this.clientsRepository.findOneOrFail(id, {relations: ["user"]});
+    }
+
+    async update(id: number, updateClientDto: UpdateClientDto) {
+        let client = await this.clientsRepository.findOneOrFail(id, {relations: ["user"]});
+        client = this.clientsRepository.merge(client, updateClientDto);
+        return this.clientsRepository.save(client);
+    }
+
+    async remove(id: number) {
+        const client = await this.clientsRepository.findOneOrFail(id);
+        return this.clientsRepository.remove(client);
+    }
 }
--- src/clients/dto/query-client.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNumber, IsOptional } from "class-validator";
-
-export class QueryClientDto {
-  @IsNumber()
-  @IsOptional()
-  clientId?: number | null;
-}
